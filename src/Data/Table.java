371ee48fcd2b2be307de276dc1117dbeba9cd4e5
Jakob-L-M <me@jakob-l-m.de>
2023-09-23 21:23:37 +0200

    Sortation during loading

--- src/Data/Table.java
+++ src/Data/Table.java
@@ -1,12 +1,19 @@
 package Data;
 
+import java.util.Arrays;
+
 public class Table {
     private final String[] columnNames;
     public String[][] values;
+    public final int numCols;
+    public String name;
 
-    public Table(String[] columnNames, String[][] values) {
+    public Table(String[] columnNames, String[][] values, String name) {
         this.columnNames = columnNames;
         this.values = values;
+        Arrays.stream(this.values).forEach(Arrays::sort);
+        this.numCols = columnNames.length;
+        this.name = name;
     }
 
     public String getColumnName(int position) {
