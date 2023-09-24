1e501de6ae837e4ff1f5bdee3ffd1da247e5e63d
Jakob-L-M <me@jakob-l-m.de>
2023-09-24 10:07:22 +0200

    Added method to transform dataset to ordered non-duplicate lists

--- src/Data/Dataset.java
+++ src/Data/Dataset.java
@@ -1,12 +1,28 @@
 package Data;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Dataset {
     public Table[] tables;
 
     public Dataset(Table[] tables) {
         this.tables = tables;
+    }
+
+    public List<List<List<String>>> sortAndRemoveDuplicates() {
+
+        List<List<List<String>>> listBasedDataset = new ArrayList<>(tables.length);
+        for (Table table: tables) {
+            List<List<String>> listBasedTable = new ArrayList<>(table.numCols);
+            for (String[] column : table.values) {
+                listBasedTable.add(Arrays.stream(column).sorted().distinct().toList());
+            }
+            listBasedDataset.add(listBasedTable);
+        }
+
+        return listBasedDataset;
     }
 
     public void printStatistics() {
